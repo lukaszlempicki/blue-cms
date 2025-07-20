b193e47ebdc4e6e9d0371a4cc412f4fa2f80049b
Łukasz Łempicki <lukasz.lempicki@wakacje.pl>
2025-07-20 22:44:42 +0200

    [blue-cms]: add: tailwind + shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
